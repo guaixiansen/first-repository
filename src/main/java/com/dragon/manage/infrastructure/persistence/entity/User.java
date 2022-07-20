5c06404876c7db084cb90684e96ee71ebc5129f7
怪哒哒 <2556843911@qq.com>
2022-07-20 10:27:08 +0800

    first

---
+++ src/main/java/com/dragon/manage/infrastructure/persistence/entity/User.java
@@ -0,0 +1,14 @@
+package com.dragon.manage.infrastructure.persistence.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+
+    private String userName;
+
+    private String password;
+
+
+}
---
+++ src/main/java/com/dragon/manage/infrastructure/persistence/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.dragon.manage.infrastructure.persistence.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+
+
+}
